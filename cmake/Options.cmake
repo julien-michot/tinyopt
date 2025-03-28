bc584b1814ca8e80102721fe159e0a83134fccfc
Julien Michot <julien.michot.fr@gmail.com>
2025-03-28 11:19:14 +0700

    Fix build options

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -5,11 +5,13 @@ option(USE_FMT "Use fmt formatting" OFF)
 option(ENABLE_FORMATTERS "Enable definion of std::formatter for streamable types, linked to TINYOPT_NO_FORMATTERS" ON)
 
 # Build Options
-#option (BUILD_TINYOPT_BINDING  "Build python binding" OFF)
+# Examples
 option(BUILD_TINYOPT_EXAMPLES "Build examples" OFF)
+# Tests
+option(BUILD_TINYOPT_TESTS "Build tests" ON)
 option(BUILD_TINYOPT_SOPHUS_EXAMPLES "Build Sophus examples, fecth it if not found" OFF)
 option(BUILD_TINYOPT_LIEPLUSPLUS_EXAMPLES "Build Lie++ examples, fecth it if not found" OFF)
-option(BUILD_TINYOPT_TESTS "Build tests" ON)
+# Packages
 option(BUILD_TINYOPT_PACKAGES "Build packages" OFF)
-
-option(BUILD_TINYOPT_DOC "Build documentation" OFF)
+# Documentation
+option(BUILD_TINYOPT_DOCS "Build documentation" OFF)
