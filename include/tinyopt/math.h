eb8631456f1cc56a1ca0891bb2884e5b1c6bd9a9
Julien Michot <julien.michot.fr@gmail.com>
2025-04-03 13:29:07 +0700

    Fix num diff when returned value is a scalar

--- include/tinyopt/math.h
+++ include/tinyopt/math.h
@@ -36,6 +36,9 @@ using Matrix = Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>;
 template <typename Scalar, int Rows = Dynamic>
 using Vector = Matrix<Scalar, Rows, 1>;
 
+template <typename T, int Cols>
+using RowVector = Matrix<T, 1, Cols, Eigen::RowMajor>;
+
 template <typename Scalar = double, int Options = 0, typename StorageIndex = int>
 using SparseMatrix = Eigen::SparseMatrix<Scalar, Options, StorageIndex>;
 
