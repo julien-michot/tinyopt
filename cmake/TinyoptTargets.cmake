7be3ec454efa5a2270a69f8df8b109ed16ea7b10
Julien Michot <julien.michot.fr@gmail.com>
2025-04-11 14:27:34 +0700

    Improve and fix TinyoptConfig.came.in to link to Eigen

--- cmake/TinyoptTargets.cmake
+++ cmake/TinyoptTargets.cmake
@@ -1,10 +1,10 @@
 
-if(TARGET tinyopt)
-  return()
+if(NOT TARGET Tinyopt)
+
+  add_library(tinyopt INTERFACE IMPORTED)
+
+  # Set the include directories (assuming headers are installed in <prefix>/include)
+  set_target_properties(tinyopt PROPERTIES
+    INTERFACE_LINK_LIBRARIES "Eigen3::Eigen" # Link against the Eigen target
+  )
 endif()
-
-add_library(tinyopt INTERFACE IMPORTED)
-
-set_target_properties(tinyopt PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include"
-)
