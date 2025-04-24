691e46227ea6ee31c5fb17fe33f265184e07b614
Julien Michot <julien.michot.fr@gmail.com>
2025-04-24 10:07:41 +0700

    Split types and math functions

--- include/tinyopt/math.h
+++ include/tinyopt/math.h
@@ -14,103 +14,11 @@
 
 #pragma once
 
-#include <iostream>
 #include <optional>
-#include <type_traits>
 
-#include <Eigen/Core>
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
-
-#include <Eigen/src/Core/util/Constants.h>
-#include <Eigen/src/SparseCore/SparseSelfAdjointView.h>
-#include <Eigen/SparseCholesky>
+#include <tinyopt/types.h>
 
 namespace tinyopt {
-
-static constexpr int Dynamic = Eigen::Dynamic;
-static constexpr int Lower = Eigen::Lower;
-static constexpr int Upper = Eigen::Upper;
-static constexpr int Infinity = Eigen::Infinity;
-
-using Index = Eigen::Index;
-
-template <typename Scalar, int Rows = Dynamic, int Cols = Dynamic, int Options = 0,
-          int MaxRows = Rows, int MaxCols = Cols>
-using Matrix =
-    std::conditional_t<Rows != 1 || (Rows == 1 && Cols == 1),
-                       Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>,
-                       Eigen::Matrix<Scalar, Rows, Cols, Eigen::RowMajor, MaxRows, MaxCols>>;
-
-template <typename Scalar, int Rows = Dynamic>
-using Vector = Matrix<Scalar, Rows, 1>;
-
-template <typename T, int Cols>
-using RowVector = Matrix<T, 1, Cols, Eigen::RowMajor>;
-
-template <typename Scalar = double, int Options = 0, typename StorageIndex = int>
-using SparseMatrix = Eigen::SparseMatrix<Scalar, Options, StorageIndex>;
-
-using SparseMatrixf = SparseMatrix<float>;
-
-template <typename XprType, int BlockRows, int BlockCols, bool InnerPanel>
-using Block = Eigen::Block<XprType, BlockRows, BlockCols, InnerPanel>;
-template <typename T>
-using MatrixBase = Eigen::MatrixBase<T>;
-template <typename T>
-using DenseBase = Eigen::DenseBase<T>;
-template <typename T>
-using ArrayBase = Eigen::ArrayBase<T>;
-
-// Let's define some common matrix and vectors
-using MatX = Matrix<double, Dynamic, Dynamic>;
-using MatXf = Matrix<float, Dynamic, Dynamic>;
-
-using Mat1 = Matrix<double, 1, 1>;
-using Mat2 = Matrix<double, 2, 2>;
-using Mat3 = Matrix<double, 3, 3>;
-using Mat4 = Matrix<double, 4, 4>;
-using Mat5 = Matrix<double, 5, 5>;
-using Mat6 = Matrix<double, 6, 6>;
-
-using Mat1f = Matrix<float, 1, 1>;
-using Mat2f = Matrix<float, 2, 2>;
-using Mat3f = Matrix<float, 3, 3>;
-using Mat4f = Matrix<float, 4, 4>;
-using Mat5f = Matrix<float, 5, 5>;
-using Mat6f = Matrix<float, 6, 6>;
-
-using VecX = Vector<double, Dynamic>;
-using Vec1 = Vector<double, 1>;
-using Vec2 = Vector<double, 2>;
-using Vec3 = Vector<double, 3>;
-using Vec4 = Vector<double, 4>;
-using Vec5 = Vector<double, 5>;
-using Vec6 = Vector<double, 6>;
-using Vec12 = Vector<double, 12>;
-using Vec21 = Vector<double, 21>;
-
-using VecXf = Vector<float, Dynamic>;
-using Vec1f = Vector<float, 1>;
-using Vec2f = Vector<float, 2>;
-using Vec3f = Vector<float, 3>;
-using Vec4f = Vector<float, 4>;
-using Vec5f = Vector<float, 5>;
-using Vec6f = Vector<float, 6>;
-using Vec12f = Vector<float, 12>;
-using Vec21f = Vector<float, 21>;
-
-using Mat2X = Matrix<double, 2, Dynamic>;
-using Mat3X = Matrix<double, 3, Dynamic>;
-
-using Mat2Xf = Matrix<float, 2, Dynamic>;
-using Mat3Xf = Matrix<float, 3, Dynamic>;
-
-using Mat23 = Matrix<double, 2, 3>;
-using Mat32 = Matrix<double, 3, 2>;
-
-using Mat23f = Matrix<float, 2, 3>;
-using Mat32f = Matrix<float, 3, 2>;
 
 /**
  * @brief Computes the inverse of a dense, symmetric, semi-definite matrix.
@@ -150,14 +58,13 @@ DenseInvCov(const MatrixBase<Derived> &m) {
   if constexpr (Derived::ColsAtCompileTime == 1) {
     return MatType(m.cwiseInverse().asDiagonal());
   } else if (m.cols() == 1) {
-    return MatType(m.inverse());
-  } else {
+    return MatType(m.inverse());  // un-protected here...
+  } else if (m.cols() > 0) {
     const auto &chol = m.template selfadjointView<Upper>().ldlt();
     if (chol.info() == Eigen::Success && chol.isPositive())
       return chol.solve(MatType::Identity(m.rows(), m.cols()));
-    else
-      return std::nullopt;
   }
+  return std::nullopt;
 }
 
 /**
@@ -219,6 +126,7 @@ template <typename T>
 std::optional<SparseMatrix<typename T::Scalar>> SparseInvCov(
     const T &m, typename T::Scalar retry_with_shift_offset = typename T::Scalar(0.0)) {
   using Scalar = typename T::Scalar;
+  if (m.size() == 0) return std::nullopt;
   Eigen::SimplicialLDLT<SparseMatrix<Scalar>, Eigen::Upper> solver;
   solver.compute(m);
   if (solver.info() != Eigen::Success) {  // Decomposition failed
---
+++ include/tinyopt/types.h
@@ -0,0 +1,111 @@
+// Copyright (C) 2025 Julien Michot. All Rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+
+#include <Eigen/src/Core/util/Constants.h>
+#include <Eigen/src/SparseCore/SparseSelfAdjointView.h>
+#include <Eigen/SparseCholesky>
+
+namespace tinyopt {
+
+static constexpr int Dynamic = Eigen::Dynamic;
+static constexpr int Lower = Eigen::Lower;
+static constexpr int Upper = Eigen::Upper;
+static constexpr int Infinity = Eigen::Infinity;
+
+using Index = Eigen::Index;
+
+template <typename Scalar, int Rows = Dynamic, int Cols = Dynamic, int Options = 0,
+          int MaxRows = Rows, int MaxCols = Cols>
+using Matrix =
+    std::conditional_t<Rows != 1 || (Rows == 1 && Cols == 1),
+                       Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>,
+                       Eigen::Matrix<Scalar, Rows, Cols, Eigen::RowMajor, MaxRows, MaxCols>>;
+
+template <typename Scalar, int Rows = Dynamic>
+using Vector = Matrix<Scalar, Rows, 1>;
+
+template <typename T, int Cols>
+using RowVector = Matrix<T, 1, Cols, Eigen::RowMajor>;
+
+template <typename Scalar = double, int Options = 0, typename StorageIndex = int>
+using SparseMatrix = Eigen::SparseMatrix<Scalar, Options, StorageIndex>;
+
+using SparseMatrixf = SparseMatrix<float>;
+
+template <typename XprType, int BlockRows, int BlockCols, bool InnerPanel>
+using Block = Eigen::Block<XprType, BlockRows, BlockCols, InnerPanel>;
+template <typename T>
+using MatrixBase = Eigen::MatrixBase<T>;
+template <typename T>
+using DenseBase = Eigen::DenseBase<T>;
+template <typename T>
+using ArrayBase = Eigen::ArrayBase<T>;
+
+// Let's define some common matrix and vectors
+using MatX = Matrix<double, Dynamic, Dynamic>;
+using MatXf = Matrix<float, Dynamic, Dynamic>;
+
+using Mat1 = Matrix<double, 1, 1>;
+using Mat2 = Matrix<double, 2, 2>;
+using Mat3 = Matrix<double, 3, 3>;
+using Mat4 = Matrix<double, 4, 4>;
+using Mat5 = Matrix<double, 5, 5>;
+using Mat6 = Matrix<double, 6, 6>;
+
+using Mat1f = Matrix<float, 1, 1>;
+using Mat2f = Matrix<float, 2, 2>;
+using Mat3f = Matrix<float, 3, 3>;
+using Mat4f = Matrix<float, 4, 4>;
+using Mat5f = Matrix<float, 5, 5>;
+using Mat6f = Matrix<float, 6, 6>;
+
+using VecX = Vector<double, Dynamic>;
+using Vec1 = Vector<double, 1>;
+using Vec2 = Vector<double, 2>;
+using Vec3 = Vector<double, 3>;
+using Vec4 = Vector<double, 4>;
+using Vec5 = Vector<double, 5>;
+using Vec6 = Vector<double, 6>;
+using Vec12 = Vector<double, 12>;
+using Vec21 = Vector<double, 21>;
+
+using VecXf = Vector<float, Dynamic>;
+using Vec1f = Vector<float, 1>;
+using Vec2f = Vector<float, 2>;
+using Vec3f = Vector<float, 3>;
+using Vec4f = Vector<float, 4>;
+using Vec5f = Vector<float, 5>;
+using Vec6f = Vector<float, 6>;
+using Vec12f = Vector<float, 12>;
+using Vec21f = Vector<float, 21>;
+
+using Mat2X = Matrix<double, 2, Dynamic>;
+using Mat3X = Matrix<double, 3, Dynamic>;
+
+using Mat2Xf = Matrix<float, 2, Dynamic>;
+using Mat3Xf = Matrix<float, 3, Dynamic>;
+
+using Mat23 = Matrix<double, 2, 3>;
+using Mat32 = Matrix<double, 3, 2>;
+
+using Mat23f = Matrix<float, 2, 3>;
+using Mat32f = Matrix<float, 3, 2>;
+
+}  // namespace tinyopt
