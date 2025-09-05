06be7b3f04d20b520307deff0770e9d061fc1625
Julien Michot <julien.michot.fr@gmail.com>
2025-09-05 09:48:12 +0700

    Add cuda compiler

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -7,31 +7,40 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON) # generate compile_commands.json for clang
 # Check the C++ compiler, define flags
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   message(STATUS "Using GCC C++ compiler")
-  set(COMPILER_FLAGS -fPIC -Wall -Wextra -Werror -fdiagnostics-color=always ) # -Wconversion
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fPIC>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wall>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wextra>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Werror>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fdiagnostics-color=always>)
+  # TODO -Wconversion
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   message(STATUS "Using Clang C++ compiler")
-  set(COMPILER_FLAGS -fPIC -Wall -Wextra -Werror -fcolor-diagnostics)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fPIC>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wall>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wextra>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Werror>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fcolor-diagnostics>)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   message(STATUS "Using MSVC C++ compiler")
-  set(COMPILER_FLAGS /W3 /wd5054) # 5054 is for Eigen, TODO:/WX ?
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/W3>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/wd5054>) # 5054 is for Eigen
+  # TODO /WX
 else()
   message(STATUS "Unknown C++ compiler: ${CMAKE_CXX_COMPILER_ID}")
 endif()
-# Add the compiler flags
-add_compile_options(${COMPILER_FLAGS})
 
 # ASAN with ConfigType Address Sanitizer (use -DCMAKE_BUILD_TYPE=ASAN)
 list(APPEND CMAKE_CONFIGURATION_TYPES ASAN)
 
 if(CMAKE_BUILD_TYPE MATCHES "ASAN")
-  add_compile_options("-fsanitize=address")
-  add_compile_options("-fsanitize-address-use-after-scope")
-  add_compile_options("-fno-optimize-sibling-calls")
-  add_compile_options("-fno-omit-frame-pointer")
-  add_compile_options("-O1")
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fsanitize=address>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fsanitize-address-use-after-scope>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fno-optimize-sibling-calls>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-fno-omit-frame-pointer>)
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-O1>)
   link_libraries("-fsanitize=address")
 elseif(CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo")
-  add_compile_options("-O2")
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-O2>)
 elseif(CMAKE_BUILD_TYPE MATCHES "Release")
-  add_compile_options("-O3")
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-O3>)
 endif()
