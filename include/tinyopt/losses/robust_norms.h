1b6468b3984e6c08bc2df9ae673f1c42fc77bfa9
Julien Michot <julien.michot.fr@gmail.com>
2026-01-24 13:24:22 +0700

    Fix Copyright Headers

--- include/tinyopt/losses/robust_norms.h
+++ include/tinyopt/losses/robust_norms.h
@@ -1,18 +1,7 @@
-// Copyright (C) 2025 Julien Michot. All Rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2026 Julien Michot.
+// SPDX-License-Identifier: Apache-2.0
 
-/// If you land here, put your shades one because those fat functions are still hurting my eyes as
+/// If you land here, put your shades on because those fat functions are still hurting my eyes as
 /// of today.
 
 #pragma once
